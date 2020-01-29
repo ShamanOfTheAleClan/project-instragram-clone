87b9dc1f8aaaf704849b54b9439a8e4774d5402d
ShamanOfTheAleClan <paulius.sviackevicius@gmail.com>
2020-01-29 19:06:10 +0200

    Login fully functional

--- front/assets/js/login.js
+++ front/assets/js/login.js
@@ -1,43 +1,46 @@
-const goToRegister = () => {
-    window.location.href = "../front/register.html";
-};
+// const goToRegister = () => {
+//     window.location.href = "../front/register.html";
+// };
 
-const login = () => {
-    let email = document.getElementById('loginEmail').value;
-    let password = document.getElementById('loginPassword').value;
-console.log('email', email, 'password', password)
-    let body = {
-        email: email,
-        password: password,
-    }
-    fetch('http://localhost:3000/instagram/user/login', {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json'
+const loginPage = document.getElementById('loginPage');
+
+if (!!loginPage) {
+    const login = () => {
+        let email = document.getElementById('loginEmail').value;
+        let password = document.getElementById('loginPassword').value;
+        console.log('email', email, 'password', password)
+        let body = {
+            email: email,
+            password: password,
         }
-    }).then((header) => {
-        console.log(header);
+        fetch('http://localhost:3000/instagram/user/login', {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((header) => {
+            console.log(header);
 
-        if (!header.ok) {
-            throw Error(header);
-        }
+            if (!header.ok) {
+                throw Error(header);
+            }
 
-        let token = header.headers.get('x-auth');
-        localStorage.setItem('x-auth', token);
-        console.log(token);
-    }).then((response) => {
-            alert('Login successful')
+            let token = header.headers.get('x-auth');
+            localStorage.setItem('x-auth', token);
+            console.log(token);
+        }).then((response) => {
+            // alert('Login successful')
             window.location.href = '../front/index.html';
-    }).catch((e) => {
-        console.log(e);
-        alert('Login failed');
-    })
+        }).catch((e) => {
+            console.log(e);
+            // alert('Login failed');
+        })
 
 
+    }
+
+    const loginBtn = document.getElementById('loginSubmitBtn');
+
+    loginBtn.addEventListener('click', login);
 }
-
-const loginBtn = document.getElementById('loginSubmitBtn')
-loginBtn.addEventListener('click', () => {
-    login()
-})

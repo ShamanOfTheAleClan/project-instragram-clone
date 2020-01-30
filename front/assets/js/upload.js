2d805f2fc8a8f492b7c03622575a9268afa0c228
skirmantek <skirmante.klimciukaite@gmail.com>
2020-01-30 18:12:00 +0200

    padarytas uploadinimo html

---
+++ front/assets/js/upload.js
@@ -0,0 +1,40 @@
+const checkifLoggedIn = () => {
+    let token = localStorage.getItem('x-auth');
+    console.log(token)
+    if (!token) {
+        window.location.href = "../login.html";
+    }
+};
+
+checkifLoggedIn();
+
+const createPost = () => {
+    
+    let newPost = document.getElementById('comment').value
+    console.log(postId)
+    let token = localStorage.getItem('x-auth');
+    let body = {
+        comment: newComment,
+        postId:postId
+    }
+    
+    fetch('http://localhost:3000/instagram/comment/create', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'x-auth': token,
+            'Content-Type': 'application/json'
+        }
+    }).then((header) => {
+        //console.log(header);
+        if (!header.ok) {
+            throw Error(header);
+        }
+    }).then((response) => {
+        alert('Item added successfully')
+    }).catch((e) => {
+       // console.log(e);
+        alert('Adding failed');
+    })
+   
+};
